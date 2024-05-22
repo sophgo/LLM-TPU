6a38dc7bedfdd76e3f3051b7e3a96dd65b1c5063
ZIFENG278 <zifengzhang18@gmail.com>
2024-05-22 15:07:09 +0800

    perf: a new gradio web_demo for Llama3 and fix output no end error

--- models/Llama3/python_demo/web_demo.py
+++ models/Llama3/python_demo/web_demo.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 import time
 import gradio as gr
-import mdtex2html
 from pipeline import Llama3
 import argparse
 
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model_path', type=str, required=True, help='path to the bmodel file')
 parser.add_argument('-t', '--tokenizer_path', type=str, default="../support/token_config", help='path to the tokenizer file')
 parser.add_argument('-d', '--devid', type=str, default='0', help='device ID to use')
-parser.add_argument('--enable_history', action='store_true', help="if set, enables storing of history memory.")
+parser.add_argument('--enable_history', type=bool, default=True, help="if set, enables storing of history memory.")
 parser.add_argument('--temperature', type=float, default=1.0, help='temperature scaling factor for the likelihood distribution')
 parser.add_argument('--top_p', type=float, default=1.0, help='cumulative probability of token words to consider as a set of candidates')
 parser.add_argument('--repeat_penalty', type=float, default=1.0, help='penalty for repeated tokens')
@@ -21,102 +21,105 @@ args = parser.parse_args()
 
 model = Llama3(args)
 
-def postprocess(self, y):
-    if y is None:
-        return []
-    for i, (message, response) in enumerate(y):
-        y[i] = (
-            None if message is None else mdtex2html.convert((message)),
-            None if response is None else mdtex2html.convert(response),
+
+def gr_update_history():
+    if model.model.token_length >= model.SEQLEN:
+        # print("... (reach the maximal length)", flush=True, end='')
+        gr.Warning("reach the maximal length, Llama3 would clear all history record")
+        model.history = [model.system]
+    else:
+        model.history.append({"role": "assistant", "content": model.answer_cur})
+
+
+def gr_user(user_input, history):
+    model.input_str = user_input
+    return "", history + [[user_input, None]]
+
+
+def gr_chat(history):
+    """
+    Stream the prediction for the given query.
+    """
+    tokens = model.encode_tokens()
+
+    # check tokens
+    if not tokens:
+        gr.Warning("Sorry: your question is empty!!")
+        return
+    if len(tokens) > model.SEQLEN:
+        gr.Warning(
+            "The maximum question length should be shorter than {} but we get {} instead.".format(
+                model.SEQLEN, len(tokens)
+            )
         )
-    return y
+        gr_update_history()
 
-def parse_text(text):
-    """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
-    lines = text.split("\n")
-    lines = [line for line in lines if line != ""]
-    count = 0
-    for i, line in enumerate(lines):
-        if "```" in line:
-            count += 1
-            items = line.split('`')
-            if count % 2 == 1:
-                lines[i] = f'<pre><code class="language-{items[-1]}">'
-            else:
-                lines[i] = f'<br></code></pre>'
-        else:
-            if i > 0:
-                if count % 2 == 1:
-                    line = line.replace("`", "\`")
-                    line = line.replace("<", "&lt;")
-                    line = line.replace(">", "&gt;")
-                    line = line.replace(" ", "&nbsp;")
-                    line = line.replace("*", "&ast;")
-                    line = line.replace("_", "&lowbar;")
-                    line = line.replace("-", "&#45;")
-                    line = line.replace(".", "&#46;")
-                    line = line.replace("!", "&#33;")
-                    line = line.replace("(", "&#40;")
-                    line = line.replace(")", "&#41;")
-                    line = line.replace("$", "&#36;")
-                lines[i] = "<br>" + line
-    text = "".join(lines)
-    return text
+    model.answer_cur = ""
+    model.answer_token = []
+    token_num = 0
 
+    first_start = time.time()
+    token = model.model.forward_first(tokens)
+    first_end = time.time()
 
-def gen(input, history):
-    i = 0
-    history.append((input, ''))
-    res = ''
-    while i < 10:
-        i += 1
-        res += str(i)
-        time.sleep(0.05)
-        history[-1] = (input, res)
-        yield res, history
+    history[-1][1] = ""
+    full_word_tokens = []
+
+    while token not in model.EOS and model.model.token_length < model.SEQLEN:
+        full_word_tokens.append(token)
+        t_word = model.tokenizer.decode(full_word_tokens, skip_special_tokens=True)
+
+        if "�" in t_word:
+            token = model.model.forward_next()
+            token_num += 1
+            continue
+
+        model.answer_token += full_word_tokens
+
+        history[-1][1] += t_word
+        full_word_tokens = []
+        yield history
+        token = model.model.forward_next()
+        token_num += 1
+
+    next_end = time.time()
+    first_duration = first_end - first_start
+    next_duration = next_end - first_end
+    tps = token_num / next_duration
+
+    print()
+    print(f"FTL: {first_duration:.3f} s")
+    print(f"TPS: {tps:.3f} token/s")
+
+    model.answer_cur = model.tokenizer.decode(model.answer_token)
+    gr_update_history()
 
 
-def predict(input, chatbot, max_length, top_p, temperature, history):
-
-    chatbot.append((parse_text(input), ""))
-    for response, history in model.stream_predict(input):
-        chatbot[-1] = (parse_text(input), parse_text(response))
-        yield chatbot, history
+def reset():
+    model.clear()
+    return [[None, None]]
 
 
-def reset_user_input():
-    return gr.update(value='')
 
-
-def reset_state():
-    return [], [], None
-
-gr.Chatbot.postprocess = postprocess
-
+description = """
+# Llama3 TPU 🏁 
+"""
 with gr.Blocks() as demo:
-    gr.HTML("""<h1 align="center">Llama3-TPU</h1>""")
-
-    chatbot = gr.Chatbot()
+    gr.Markdown(description)
     with gr.Row():
-        with gr.Column(scale=4):
-            with gr.Column(scale=12):
-                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
-                    container=False)
-            with gr.Column(min_width=32, scale=1):
-                submitBtn = gr.Button("Submit", variant="primary")
-        with gr.Column(scale=1):
-            emptyBtn = gr.Button("Clear History")
-            max_length = gr.Slider(1, 20, value=1, step=1.0, label="Maximum length", interactive=True)
-            top_p = gr.Slider(0, 1, value=0.8, step=0.01, label="Top P", interactive=True)
-            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+        with gr.Column():
+            chatbot = gr.Chatbot(label="Llama3", height=1050)
 
-    history = gr.State([])
+            with gr.Row():
+                user_input = gr.Textbox(show_label=False, placeholder="Ask Llama3", lines=1, min_width=300, scale=6)
+                submitBtn = gr.Button("Submit", variant="primary", scale=1)
+                emptyBtn = gr.Button(value="Clear", scale=1)
 
-    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history],
-                    [chatbot, history], show_progress=True)
-    submitBtn.click(reset_user_input, [], [user_input])
+    user_input.submit(gr_user, [user_input, chatbot], [user_input, chatbot]).then(gr_chat, chatbot, chatbot)
+    # clear_history.
+    submitBtn.click(gr_user, [user_input, chatbot], [user_input, chatbot]).then(gr_chat, chatbot, chatbot)
 
-    emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+    emptyBtn.click(reset, outputs=[chatbot])
 
-demo.queue().launch(share=True, server_name="0.0.0.0", server_port=8003, inbrowser=True)
+demo.queue(max_size=20).launch(share=False, server_name="0.0.0.0", inbrowser=True, server_port=8003)
 
