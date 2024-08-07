4dd072fb29ec600927db569c05b8502409074355
yi.chu <yi.chu@sophgo.com>
2024-08-07 21:39:38 +0800

    fix lib_soc

--- models/Qwen2/support/include/bmdef.h
+++ models/Qwen2/support/include/bmdef.h
@@ -152,14 +152,11 @@ typedef struct {
 typedef struct {
   memory_t instruction_mem;             // bdc_cmd + hau_cmd + dynamic_ir
   memory_t variable_instruction_mem;    // gdma_cmd + sdma_cmd
-  memory_t neuron_mem;                  // neuron + middle_buffer + dynamic_output
+  memory_t neuron_mem;                  // neuron + dynamic_output
   memory_t coeff_mem;                   // coeff
   memory_t io_mem;                      // input + output
   memory_t reserved[4];
 } mem_info_t;
-
-/* define decrypt_func type for decrypt bmodel */
-typedef uint8_t *(*decrypt_func)(const uint8_t *, uint64_t, uint64_t *);
 
 #if defined(__cplusplus)
 }
