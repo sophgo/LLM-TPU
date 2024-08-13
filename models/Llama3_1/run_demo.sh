10e419c699fc817a88edc2c23e6480144cd8f9f1
yilongchan01 <yuning.chen01@sophgo.com>
2024-08-13 20:41:00 +0800

    [Llama3.1] update run_demo.sh

--- models/Llama3_1/run_demo.sh
+++ models/Llama3_1/run_demo.sh
@@ -4,10 +4,10 @@ if [ ! -d "../../bmodels" ]; then
   mkdir ../../bmodels
 fi
 
-if [ ! -f "../../bmodels/llama3.1-8b_int4_1dev_seq512.bmodel" ]; then
+if [ ! -f "../../bmodels/llama3.1-8b_int8_1dev_seq512.bmodel" ]; then
   pip3 install dfss
-  python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/llama3.1-8b_int4_1dev_seq512.bmodel
-  mv llama3.1-8b_int4_1dev_seq512.bmodel ../../bmodels
+  python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/llama3.1-8b_int8_1dev_seq512.bmodel
+  mv llama3.1-8b_int8_1dev_seq512.bmodel ../../bmodels
 else
   echo "Bmodel Exists!"
 fi
@@ -24,4 +24,4 @@ fi
 # run demo
 echo $PWD
 export PYTHONPATH=$PWD/python_demo:$PYTHONPATH
-python3 python_demo/pipeline.py --model ../../bmodels/llama3.1-8b_int4_1dev_seq512.bmodel --tokenizer ./token_config --devid 0
+python3 python_demo/pipeline.py --model ../../bmodels/llama3.1-8b_int8_1dev_seq512.bmodel --tokenizer ./token_config --devid 0
