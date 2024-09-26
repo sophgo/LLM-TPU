8af0b78312dacad5127cf2414fa96e79626b211e
pengchao.hu <pengchao.hu@sophgo.com>
2024-09-26 10:03:36 +0800

    fix internvl2 prompt

--- models/InternVL2/python_demo/pipeline.py
+++ models/InternVL2/python_demo/pipeline.py
@@ -1,5 +1,4 @@
 import time
-import torch
 import argparse
 from PIL import Image
 import torchvision.transforms as T
@@ -25,51 +24,49 @@ def build_transform(input_size):
     return transform
 
 
-def load_image(image_file, input_size=448, max_num=12):
-    image = Image.open(image_file).convert('RGB')
-    transform = build_transform(input_size=input_size)
-    pixel_values = transform(image)
-    return pixel_values
-
-
 class InternVL2():
+
     def __init__(self, args):
         # devid
         self.device = args.devid
 
         # load tokenizer
         print("Load " + args.tokenizer + " ...")
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            args.tokenizer, trust_remote_code=True
-        )
+        self.tokenizer = AutoTokenizer.from_pretrained(args.tokenizer,
+                                                       trust_remote_code=True)
         self.tokenizer.decode([0])  # warm up
 
         # preprocess parameters, such as prompt & tokenizer
-        self.system_prompt = '<|system|>\n你是由上海人工智能实验室联合商汤科技开发的书生多模态大模型，英文名叫InternVL, 是一个有用无害的人工智能助手。<|end|><|user|>\n'
-        self.image_ids = [0] * 256
-
+        self.system_prompt = '<|im_start|>system\n你是由上海人工智能实验室联合商汤科技开发的书生多模态大模型，英文名叫InternVL, 是一个有用无害的人工智能助手。<|im_end|><|im_start|>user\n'
+        image_ids = [0] * 256
+        system_ids = self.tokenizer.encode(self.system_prompt + "<img>")
+        self.system_offset = len(system_ids)
+        self.system_prefix = system_ids + image_ids
+        self.image_transform = build_transform(448)
         # load model
         self.model = chat.InternVL2()
         self.model.init(self.device, args.model_path)
         self.SEQLEN = self.model.SEQLEN
         self.ID_EOS = self.tokenizer.eos_token_id
         self.ID_IM_END = self.tokenizer.convert_tokens_to_ids("<|im_end|>")
-        self.ID_END = self.tokenizer.convert_tokens_to_ids("<|end|>")
+
+    def load_image(self, image_file):
+        image = Image.open(image_file).convert('RGB')
+        pixel_values = self.image_transform(image)
+        return pixel_values
 
     def encode(self):
         if not self.image_str:
-            prompt = self.system_prompt + self.input_str + "<|end|><|assistant|>\n"
+            prompt = self.system_prompt + self.input_str + "<|im_end|><|im_start|>assistant\n"
             self.input_ids = self.tokenizer.encode(prompt)
             self.image_offset = 0
             self.pixel_values = []
             return
-        self.pixel_values = load_image(
-            self.image_str, max_num=1).flatten().tolist()
-        system_ids = self.tokenizer.encode(self.system_prompt + "<img>")
-        self.image_offset = len(system_ids)
+        self.pixel_values = self.load_image(self.image_str).flatten().tolist()
+        self.image_offset = self.system_offset
         prompt_ids = self.tokenizer.encode(
-            "</img>{}<|end|><|assistant|>\n".format(self.input_str))
-        self.input_ids = system_ids + self.image_ids + prompt_ids
+            "</img>{}<|im_end|><|im_start|>assistant\n".format(self.input_str))
+        self.input_ids = self.system_prefix + prompt_ids
 
     def chat(self):
         """
@@ -80,8 +77,7 @@ class InternVL2():
             """\n=================================================================
 1. If you want to quit, please enter one of [q, quit, exit]
 2. To create a new chat session, please enter one of [clear, new]
-================================================================="""
-        )
+=================================================================""")
         # Stop Chatting with "exit" input
         while True:
             self.input_str = input("\nQuestion: ")
@@ -97,25 +93,29 @@ class InternVL2():
             self.encode()
             # Chat
             first_start = time.time()
-            token = self.model.forward_first(
-                self.input_ids, self.pixel_values, self.image_offset)
+            token = self.model.forward_first(self.input_ids, self.pixel_values,
+                                             self.image_offset)
             first_end = time.time()
             tok_num = 1
             # Following tokens
             full_word_tokens = []
-            while token not in [self.ID_EOS, self.ID_END, self.ID_IM_END] and self.model.token_length < self.SEQLEN:
+            text = ""
+            while token not in [self.ID_EOS, self.ID_IM_END
+                                ] and self.model.token_length < self.SEQLEN:
                 full_word_tokens.append(token)
-                word = self.tokenizer.decode(
-                    full_word_tokens, skip_special_tokens=True)
+                word = self.tokenizer.decode(full_word_tokens,
+                                             skip_special_tokens=True)
                 if "�" not in word:
                     if len(full_word_tokens) == 1:
                         pre_word = word
-                        word = self.tokenizer.decode([token, token], skip_special_tokens=True)[
-                            len(pre_word):]
+                        word = self.tokenizer.decode(
+                            [token, token],
+                            skip_special_tokens=True)[len(pre_word):]
+                    text += word
                     print(word, flush=True, end="")
                     full_word_tokens = []
-                tok_num += 1
                 token = self.model.forward_next()
+                tok_num += 1
             next_end = time.time()
             first_duration = first_end - first_start
             next_duration = next_end - first_end
@@ -131,10 +131,16 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model_path', type=str,
-                        required=True, help='path to the bmodel file')
-    parser.add_argument('-t', '--tokenizer', type=str,
-                        default="../support/token_config", help='path to the tokenizer file')
+    parser.add_argument('-m',
+                        '--model_path',
+                        type=str,
+                        required=True,
+                        help='path to the bmodel file')
+    parser.add_argument('-t',
+                        '--tokenizer',
+                        type=str,
+                        default="../support/token_config",
+                        help='path to the tokenizer file')
     parser.add_argument('-d', '--devid', type=int,
                         default=0, help='device ID to use')
     args = parser.parse_args()
